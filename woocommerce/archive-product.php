d1ea471ac581f496de3b77f8be041cd151519f26
D-Bars <awotik26@gmail.com>
2024-09-03 14:24:38 +0200

    add title and description on category

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -9,7 +9,17 @@
 		</div><!-- ./col-lg-3 col-md-4 -->
 
 		<div class="col-lg-9 col-md-8">
-			Content
+			<div class="row">
+				<div class="col-12">
+					<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+						<h1 class="woocommerce-products-header__title page-title section-title h3">
+							<span><?php woocommerce_page_title(); ?></span>
+						</h1>
+					<?php endif; ?>
+				</div><!-- ./col-12 -->
+
+				<?php do_action( 'woocommerce_archive_description' ); ?>
+			</div><!-- ./row -->
 		</div><!-- ./col-lg-9 col-md-8 -->
 
 	</div><!-- ./row -->
